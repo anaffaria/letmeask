a1d15979cbffbd5b07fe1328ba57153c43aa62da
aninhafsilva <anacarolinq.ng@gmail.com>
2021-08-03 18:48:24 -0300

    finished the application

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -26,7 +26,7 @@ export function Question({
       className={cx(
         "question",
         { answered: isAnswered },
-        { highlighted: isHighlighted }
+        { highlighted: isHighlighted && !isAnswered}
       )}
     >
       <p>{content}</p>
